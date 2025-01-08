aba6fc0ad1d8547a42d828f5929c0a39dd45628c
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-09 04:13:04 +0800

    Add Swagger schema definition for User model

---
+++ src/swagger/schema.swagger.ts
@@ -0,0 +1,29 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - email
+ *         - name
+ *         - telp
+ *         - status
+ *         - department
+ *       properties:
+ *         email:
+ *           type: string
+ *           example: user@email.com
+ *         name:
+ *           type: string
+ *           example: Mochammad Syahrul Kurniawan
+ *         telp:
+ *           type: string
+ *           example: "1234567890"
+ *         status:
+ *           type: boolean
+ *           example: true
+ *         department:
+ *           type: string
+ *           example: Backend Developer
+ */
