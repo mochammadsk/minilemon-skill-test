156f800edd2630665f841381da5adbc0fdd0ed0b
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-09 01:47:44 +0800

    Add CORS, session management, and Swagger documentation setup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,45 @@
-import express, { Request, Response } from 'express';
+import cors from 'cors';
+import express from 'express';
+import session from 'express-session';
+import swaggerConfig from './configs/swagger';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
-const port = 3000;
 
+const swaggerSpec = swaggerJSDoc(swaggerConfig);
+
+// Middleware CORS & Parsing
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+  })
+);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, Express with TypeScript!');
+// Middleware Session
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
+
+// Middleware Logger
+app.use((req, _res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Call Routes
+
+export default app;
